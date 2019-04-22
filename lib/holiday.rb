4f4764e36cb7a108b8f9f2b5d1b7995cc9fed1b4
Katherine Wren <kwren@outlook.com>
2019-04-22 19:32:39 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -59,7 +59,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 holiday_hash.each do |season, holiday|
-  puts season.capitalize.to_s <<
+  puts season.capitalize.to_s << "."
     holiday.each do |holname, supplies|
       string = holname.to_s
       bothwords = string.split
